bb11359cdb2d490b7b8fd79dd1c2568a2f602f9d
pepito <pepito@frba.utn.edu.ar>
2023-06-01 09:51:12 -0300

    Segundo Commit

--- 2023-f-parcial-ValenGriggio18-master/src/Library.hs
+++ 2023-f-parcial-ValenGriggio18-master/src/Library.hs
@@ -7,5 +7,14 @@ data Nave = UnaNave {
     nombre :: String,
     durabilidad :: Number,
     escudo :: Number,
+    ataque :: Number,
     poderEspecial :: Poder
 }deriving Show
+
+movTurbo :: Nave -> Nave
+movTurbo laNave = laNave {ataque = ataque laNave + 25}
+
+reparacion :: Nave -> Nave
+reparacion laNave = laNave {ataque = ataque laNave - 30, durabilidad = durabilidad laNave + 50}
+
+superTurbo :: Nave -> Nave 
