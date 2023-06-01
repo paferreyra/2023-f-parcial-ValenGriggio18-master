09b46998c1a61da189b3f92c44fa1da958994986
pepito <pepito@frba.utn.edu.ar>
2023-06-01 11:44:32 -0300

    Commit de transicion 3

--- 2023-f-parcial-ValenGriggio18-master/src/Library.hs
+++ 2023-f-parcial-ValenGriggio18-master/src/Library.hs
@@ -10,8 +10,8 @@ data Nave = UnaNave {
     durabilidad :: Number,
     escudo :: Number,
     ataque :: Number,
-    poderEspecial :: String
-}deriving Show
+    poderEspecial :: Poder
+}deriving (Show, Eq)
 
 --Flota de ejemplo
 
@@ -38,19 +38,19 @@ velocidadTurbo laNave = laNave{escudo = escudo laNave - escudo laNave, ataque = 
 --Modelado de las naves
 
 tieFighter :: Nave
-tieFighter = UnaNave {nombre = "TIE Fighter", durabilidad = 200, escudo = 100, ataque = 50, poderEspecial = "Movimiento turbo"}
+tieFighter = UnaNave {nombre = "TIE Fighter", durabilidad = 200, escudo = 100, ataque = 50, poderEspecial = movTurbo}
 
 xWing :: Nave
-xWing = UnaNave {nombre = "X Wing", durabilidad = 300, escudo = 150, ataque = 100, poderEspecial = "Reparacion"}
+xWing = UnaNave {nombre = "X Wing", durabilidad = 300, escudo = 150, ataque = 100, poderEspecial = reparacion}
 
 darthVader :: Nave
-darthVader = UnaNave {nombre = "Nave de Darth Vader", durabilidad = 500, escudo = 300, ataque = 200, poderEspecial = "Super turbo"}
+darthVader = UnaNave {nombre = "Nave de Darth Vader", durabilidad = 500, escudo = 300, ataque = 200, poderEspecial = superTurbo}
 
 millenniumFalcon :: Nave
-millenniumFalcon = UnaNave {nombre = "Millennium Falcon", durabilidad = 1000, escudo = 500, ataque = 50, poderEspecial = "Reparacion de emergencia"}
+millenniumFalcon = UnaNave {nombre = "Millennium Falcon", durabilidad = 1000, escudo = 500, ataque = 50, poderEspecial = reparacionEmergencia}
 
 speedyWings :: Nave
-speedyWings = UnaNave {nombre = "Speedy Wings", durabilidad = 350, escudo = 100, ataque=800, poderEspecial = "Velocidad turbo" }
+speedyWings = UnaNave {nombre = "Speedy Wings", durabilidad = 350, escudo = 100, ataque=800, poderEspecial = velocidadTurbo}
 
 --Punto 2: Durabilidad total de una flota
 
@@ -59,23 +59,37 @@ calcularDurabilidad laFlota = sum(map durabilidad  laFlota)
 
 --Punto 3: Saber como queda una nave luego de ser atacada por otra
 
+dañoRecibido :: Nave -> Nave -> Number
+dañoRecibido naveAtacante naveAtacada = ataque (poderEspecial naveAtacante naveAtacante) - escudo (poderEspecial naveAtacada naveAtacada)
 
-
-dañoRecibido :: Nave -> Nave -> Poder -> Poder -> Number
-dañoRecibido naveAtacante naveAtacada elPoderAtacante elPoderAtacada  = ataque (elPoderAtacante naveAtacante) - escudo (elPoderAtacada naveAtacada)
-
-ataqueDeNave :: Nave -> Nave -> Poder -> Poder -> Nave
-ataqueDeNave naveAtacante naveAtacada elPoderAtacante elPoderAtacada
-    |escudo naveAtacada < ataque naveAtacante = naveAtacada{durabilidad = durabilidad naveAtacada - dañoRecibido naveAtacante naveAtacada elPoderAtacante elPoderAtacada}
+ataqueDeNave :: Nave -> Nave -> Nave
+ataqueDeNave naveAtacante naveAtacada
+    |escudo naveAtacada < ataque naveAtacante && dañoRecibido naveAtacante naveAtacada < durabilidad naveAtacada = naveAtacada{durabilidad = durabilidad naveAtacada - dañoRecibido naveAtacante naveAtacada}
+    |escudo naveAtacada < ataque naveAtacante && dañoRecibido naveAtacante naveAtacada >= durabilidad naveAtacada = naveAtacada{durabilidad = 0}
     |otherwise = naveAtacada
-
-
-
 
 --Punto 4: Averiguar si una nave esta fuera de combate
 
 fueraDeCombate :: Nave -> Bool
 fueraDeCombate laNave = durabilidad laNave == 0
+
+--Punto 5: Averiguar como queda una flota enemiga
+
+escudoMenor200 :: Nave -> Bool
+escudoMenor200 laNave = escudo laNave < 200
+
+navesDebiles :: Flota -> Flota
+navesDebiles = filter escudoMenor200
+
+ataqueDesignado :: Number -> Nave -> Bool
+ataqueDesignado valorAtaque laNave = ataque laNave > valorAtaque
+
+navesPeligrosas :: Number -> Flota -> Flota
+navesPeligrosas valorDesignado = filter (ataqueDesignado valorDesignado)
+
+navesFueraDeCombate :: Flota -> Flota
+navesFueraDeCombate laFlota = filter (fueraDeCombate (ataqueDeNave naveAtacante naveAtacada))
+
 
 
 
